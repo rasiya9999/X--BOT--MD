564ded1f3d0d8716cff64a36979b3db8891bc8bb
rasiya9999 <133758133+rasiya9999@users.noreply.github.com>
2025-05-06 23:23:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -69,7 +69,7 @@ module.exports = {
 	READ_MESSAGES: toBool(process.env.READ_MESSAGES || "false"),
 	REJECT_CALL: toBool(process.env.REJECT_CALL || "false"),
 	REJECT_CALL_MSG: process.env.REJECT_CALL_MSG || "_Calls are not allowed. Please don’t call again!._",
-	SESSION_ID: process.env.SESSION_ID || "",
+	SESSION_ID: process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:16abdee99afbbac4fdd07165b133a7ea",
 	START_MSG: toBool(process.env.START_MSG || "true"),
 	STICKER_DATA: process.env.STICKER_DATA || `X BOT MD;ASWIN SPARKY`,
 	SUDO: process.env.SUDO || "917012984396",
